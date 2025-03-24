55d4d86fd627d1affedda0201959b1f102dbd3f1
abulo.hoo <abulo.hoo@gmail.com>
2025-03-24 10:06:19 +0800

    style: 移除 ElementPlus 的全局配置
    移除了 ElementPlus 的 size 和 zIndex 全局配置，使用默认样式。

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,4 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import App from './App.vue'
 const app = createApp(App)
-app.use(ElementPlus, { size: 'small', zIndex: 3000 }).mount('#app')
+app.use(ElementPlus).mount('#app')
