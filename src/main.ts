32b98cc63504a17975a7686d69eeaa11710af90a
abulo.hoo <abulo.hoo@gmail.com>
2025-03-24 08:41:12 +0800

    feat: 集成 ElementPlus 组件库
    - 在 main.ts 中引入 ElementPlus
    - 配置 ElementPlus 的默认样式和 zIndex
    - 删除了 App.vue 中的默认内容
    - 移除了 style.css 文件
    - 更新了 tsconfig.json，添加 ElementPlus 类型引用

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus, { size: 'small', zIndex: 3000 }).mount('#app')
