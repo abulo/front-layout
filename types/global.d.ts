02c946282546b6d6dc886db22cece04bab7f839a
abulo.hoo <abulo.hoo@gmail.com>
2025-03-26 08:50:02 +0800

    feat(utils): 添加 localforage 离线存储工具
    - 新增 localforage 封装类，提供过期时间设置和完整类型提示
    - 在 package.json 中添加 localforage 依赖
    - 更新样式文件，将 --pure- 前缀改为 -front-layout- 前缀
    - 修改网页标题为 front-layout-thin

--- types/global.d.ts
+++ types/global.d.ts
@@ -51,7 +51,7 @@ declare global {
 
   /**
    * 全局自定义环境变量的类型声明
-   * @see {@link https://pure-admin.cn/pages/config/#%E5%85%B7%E4%BD%93%E9%85%8D%E7%BD%AE}
+   * @see {@link https://front-layout.cn/pages/config/#%E5%85%B7%E4%BD%93%E9%85%8D%E7%BD%AE}
    */
   interface ViteEnv {
     VITE_PORT: number;
