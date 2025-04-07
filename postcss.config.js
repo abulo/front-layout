9096723eadf721c7595d7b578ff7edbfc2a9c50e
abulo.hoo <abulo.hoo@gmail.com>
2025-04-07 16:49:57 +0800

    feat(env): 添加开发环境加密配置
    - 在 .env 文件中添加了 VITE_ENCRYPT、VITE_ENCRYPT_SECRET_KEY 和 VITE_ENCRYPT_IV 配置项
    - 在 build/utils.ts 和 types/global.d.ts 中添加了相应的环境变量声明
    - 在 package.json 中添加了 crypto-js 依赖

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@
 /** @type {import('postcss-load-config').Config} */
 export default {
   plugins: {
+    autoprefixer: {},
     ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {})
   }
 };
